b986700f6cb34e554dd1ecdb032d337d2092357e
sin kim <bye_ok@naver.com>
2022-08-17 10:12:18 +0900

    [220817 - working]

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -18,5 +18,16 @@ function login() {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(req),
-  });
+  })
+  .then((res) => res.json())
+  .then((res) => {
+    if (res.success) {
+      location.href = '/';
+    } else {
+      console.log(res.msg);
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  })
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const User = require("../../models/User");
+const UserStorage = require("../../models/UserStorage");
+
 const output = {
   home: (req, res) => {
     res.render('home/index');
@@ -11,7 +14,9 @@ const output = {
 
 const process = {
   login: (req, res) => {
-    console.log(req.body);
+    const user = new User(req.body);
+    const response = user.login();
+    return res.json(response);
   }
 }
 
