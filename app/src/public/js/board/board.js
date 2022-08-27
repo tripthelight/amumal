fbe55c1f4ad29099c4cb279a270a77d0d1dd6b27
Sin Kim <bye_ok@naver.com>
2022-08-27 12:25:24 +0900

    [220827 - file system 으로 실시간 write 기능 구현]

--- app/src/public/js/board/board.js
+++ app/src/public/js/board/board.js
@@ -5,10 +5,19 @@ const btnBoard = document.querySelector('#btnBoard');
 
 btnBoard.addEventListener('click', boardWriteEvent);
 
+// input label click
 function boardWriteEvent() {
-
   if (!board.value) return alert('내용을 입력해 주세요');
+  fetchEvent();
+}
 
+// input enter key
+function boardWriteEventEnder(e) {
+  if (!board.value) return alert('내용을 입력해 주세요');
+  if (e.keyCode == 13) fetchEvent();
+}
+
+function fetchEvent() {
   const req = {
     board: board.value
   };
@@ -22,49 +31,70 @@ function boardWriteEvent() {
   })
   .then((res) => res.json())
   .then((res) => {
-    if (res.success) {
-      //
-    } else {
-      console.log(res.msg);
-    }
     valueInit();
   })
-  .catch((err) => {
-    console.log(err);
-  })
+  .catch(console.log);
 }
 
 // 아무말 json 받아오기
-// fetch('http://localhost:3000/boardList')
-//   .then((response) => response.json()) 
-//   .then((json) => {
-//     viewBoardList(json);
-//   });
+let startShowingMessage = setInterval(async () => {
+  await fetch('http://localhost:3000/boardList')
+    .then((response) => response.json()) 
+    .then((json) => {
+      viewBoardList(json)
+    })
+    .catch(console.log);
+}, 1);
 
-async function startShowingMessage(){
-  // setInterval(async function(){
-    await fetch('http://localhost:3000/boardList')
-      .then((response) => response.json()) 
-      .then((json) => {
-        viewBoardList(json)
-      })
-      .catch((error) => {console.log(error);});
-  // }, 1000);
-}
-startShowingMessage();
 
 let viewBoardList = (boardList) => {
+
+  const BOARD_WRAP = document.querySelector('.board-list');
+  if (BOARD_WRAP) {
+    let board_list = BOARD_WRAP.querySelectorAll('li');
+
+    // 최초 접속
+    if (board_list.length == 0) {
+      let HTML = '';
+      boardList.description.map(item => {
+        HTML += `<li>${item}</li>`;
+        BOARD_WRAP.innerHTML = HTML;
+      });
+    }
+
+    // 접속 이후
+    if (board_list.length > 0) {
+      if (boardList.description.length != board_list.length) {
+        console.log('다름');
+
+        for (let i = 0; i < board_list.length; i++) {
+          board_list[i].remove();
+        }
+
+        let HTML = '';
+        boardList.description.map(item => {
+          HTML += `<li>${item}</li>`;
+          BOARD_WRAP.innerHTML = HTML;
+        });
+      }
+    }
+  }
+
+
+
+
+
+
+  /*
   let HTML = '';
-  const BOARD_LIST = document.querySelector('.board-list');
   if (BOARD_LIST) {
     boardList.description.map(item => {
       HTML += `<li>${item}</li>`;
       BOARD_LIST.innerHTML = HTML;
     });
   }
+  */
 }
 
-// valueInit
-function valueInit() {
-  board.value = '';
-}
+// value init
+let valueInit = () => board.value = '';
